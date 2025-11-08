a592b517ff4a5b07b68f99f2af47ed7664163732
esther apalara <apalaraesther@gmail.com>
2025-11-08 16:42:13 +0000

    Complete fundamentals and core concepts of react project

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "components./MainContent";
+import Footer from "component./Footer";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
