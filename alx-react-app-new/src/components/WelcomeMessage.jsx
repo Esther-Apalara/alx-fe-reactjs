21075c256932e90fe94fdfe9055e5d9630ba5a17
esther apalara <apalaraesther@gmail.com>
2025-11-09 21:24:35 +0000

    fix: correct JSX structure in WelcomeMessage component

--- alx-react-app-new/src/components/WelcomeMessage.jsx
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -1,10 +1,10 @@
 function WelcomeMessage() {
   return (
-    <div>
+    <><div>
       <h1>Hello everyone, I am learning React at ALX!</h1>
       <p>This is a simple JSX component.</p>
       <p>I am learning about JSX!</p>
-    </div>
+    </div></>
   );
 }
 
