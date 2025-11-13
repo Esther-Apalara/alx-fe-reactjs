0d0dca9b2dfdc6d8b2636faf10d3cd0b20ea3df0
esther apalara <apalaraesther@gmail.com>
2025-11-13 21:14:53 +0000

    Fix recipeStore import, add setRecipes, and update components

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,25 +1,27 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
-export const useRecipeStore = create(set => ({
+export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   updateRecipe: (updatedRecipe) =>
-    set(state => ({
-      recipes: state.recipes.map(r =>
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
         r.id === updatedRecipe.id ? updatedRecipe : r
-      )
+      ),
     })),
   deleteRecipe: (id) =>
-    set(state => ({
-      recipes: state.recipes.filter(r => r.id !== id)
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
     })),
   searchTerm: '',
   setSearchTerm: (term) => set({ searchTerm: term }),
   filteredRecipes: [],
   filterRecipes: () =>
-    set(state => ({
-      filteredRecipes: state.recipes.filter(recipe =>
+    set((state) => ({
+      filteredRecipes: state.recipes.filter((recipe) =>
         recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-      )
-    }))
+      ),
+    })),
 }));
