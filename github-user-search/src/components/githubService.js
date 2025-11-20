5d99dd3fba85fa0a740691a4ce31179c52e5d633
esther apalara <apalaraesther@gmail.com>
2025-11-20 00:07:57 +0000

    feat: upgrade Search component with advanced API and UI improvements

--- github-user-search/src/components/githubService.js
+++ github-user-search/src/components/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
+  const response = await axios.get(`https://api.github.com/search/users?q=${username}`);
+  return response.data.items; // returns an array of users
 };
