562453343eccb482ecc7aae9ee1fac0ff6ba8ed0
esther apalara <apalaraesther@gmail.com>
2025-11-21 22:50:25 +0000

    Fix advanced search query for ALX checker

--- github-user-search/src/components/githubService.js
+++ github-user-search/src/components/githubService.js
@@ -1,19 +1,16 @@
 import axios from "axios";
 
-// Function to fetch users with advanced search
 export const fetchUserData = async (username, location = "", minRepos = "") => {
   try {
-    // Construct query string
     let query = "";
     if (username) query += `${username}`;
     if (location) query += `+location:${location}`;
-    if (minRepos) query += `+repos:>${minRepos}`;
+    if (minRepos) query += `+repos:>=${minRepos}`;
 
-    // GitHub Search API endpoint
     const url = `https://api.github.com/search/users?q=${query}`;
 
     const response = await axios.get(url);
-    return response.data.items; // return array of users
+    return response.data.items;
   } catch (error) {
     console.error("Error fetching users:", error);
     throw error;
