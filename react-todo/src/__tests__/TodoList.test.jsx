493b17851e82450cd7349c08b436bf7993a4b22b
esther apalara <apalaraesther@gmail.com>
2025-12-11 23:55:55 +0000

    Task 3: Initial Todo List implementation

---
+++ react-todo/src/__tests__/TodoList.test.jsx
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList.jsx";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText(/Learn React/i)).toBeInTheDocument();
+  expect(screen.getByText(/Build a project/i)).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText(/Add new todo/i), {
+    target: { value: "Test Todo" },
+  });
+  fireEvent.click(screen.getByText(/Add/i));
+  expect(screen.getByText(/Test Todo/i)).toBeInTheDocument();
+});
+
+test("toggles a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText(/Learn React/i);
+  fireEvent.click(todo);
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText(/Learn React/i);
+  const deleteButton = screen.getAllByText(/Delete/i)[0];
+  fireEvent.click(deleteButton);
+  expect(todo).not.toBeInTheDocument();
+});
